9ca08d3f31bf68b079e90a6ddccf6016b8ca3da2
Ignasi Pardo <60847464+KuronoaScarlet@users.noreply.github.com>
2020-02-19 20:35:23 +0100

    Update Main_4.cpp

--- Deberes_Punteros/Ejercicio_4/Main_4.cpp
+++ Deberes_Punteros/Ejercicio_4/Main_4.cpp
@@ -1,25 +1,50 @@
 #include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 
-
+void absSwap(int* arr1, int sizeArr1, int* arr2, int sizeArr2) 
+{
+	for (int i = 0; i < sizeArr1; i++)
+	{
+		arr1[i] = abs(arr2[i]);
+	}
+}
 
 int main()
 {
 	int* arr1 = (int*)calloc(20, sizeof(int));
 	int* arr2 = (int*)malloc(20 * sizeof(int));
 
-	int size1 = sizeof(arr1);
-	int size2 = sizeof(arr2);
+	int size1 = 20;
+	int size2 = 20;
+
+
+	std::cout << "Introduce 20 valores, positivos, negativos o 0:" << std::endl;
 
 	for (int i = 0; i < size2; i++)
 	{
-		scanf_s("%d", arr2[i]);
+		std::cin >> arr2[i];
 	}
+
+	absSwap(arr1, size1, arr2, size2);
+
+	std::cout <<"Array1:"<< std::endl;
+
+	for (int i = 0; i < size1; i++)
+	{
+		std::cout << arr1[i];
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "Array2:" << std::endl;
 
 	for (int i = 0; i < size2; i++)
 	{
-		printf("%d ", arr2[i]);
+		std::cout << arr2[i];
+		std::cout << " ";
 	}
+	std::cout << std::endl;
 
 	free (arr1);
 	free (arr2);
