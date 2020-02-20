c5c0e6303a27d1ad17b4053cdf94604c4ca14e17
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2020-02-20 09:37:44 +0100

    Update Main_5.cpp

--- Deberes_Punteros/Ejercicio_5/Main_5.cpp
+++ Deberes_Punteros/Ejercicio_5/Main_5.cpp
@@ -3,23 +3,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+void printCharPtr(char* myString)
+{
+	std::cout << myString << std::endl;
+}
+
+int sizeWord(char* word)
+{
+	int num = strlen(word);
+
+	return num;
+}
+
+void concat(char* word1, char* word2, char* result) 
+{
+	result = word1;
+	result[strlen(word1)] = (char)word2;
+	result[strlen(word1) + strlen(word2)] = '\0';
+}
 
 int main()
 {
-	char* word1;
-	char* word2;
-	char* conc = (char*)malloc(sizeof(char));
+	char word1[50];
+	char word2[50];
+	int size1 = 0, size2 = 0;
 
 	std::cout << "Intruduce la primera palabra: " << std::endl;
 	std::cin >> word1;
-	
 
-	std::cout << "introduce la segunda palabra: " << std::endl;
-	std::cin >> word2;
+	size1 = sizeWord(word1);
 	
+	std::cout << "Introduce la segunda palabra: " << std::endl;
+	std::cin >> word2;
+
+	size2 = sizeWord(word2);
+
+	char* conc = (char*)malloc((size1 + size2 + 1)*sizeof(char));
+
+	concat(word1, word2, conc);
+	
+	printCharPtr(conc);
+
 	std::cout << word1 << std::endl;
 	std::cout << word2 << std::endl;
-
 
 	free(conc);
 
