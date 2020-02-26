6fb0266b4577f9fda025460aed39c7ce5fae6c4f
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2020-02-26 17:24:01 +0100

    Update Main.cpp

--- SDL_Lessons/SDL_Lesson1/Main.cpp
+++ SDL_Lessons/SDL_Lesson1/Main.cpp
@@ -117,25 +117,6 @@ int main(int argc, char* args[])
 			SDL_UpdateWindowSurface(gWindow);
 			SDL_Delay(2000);
 
-			//Main loop flag
-			bool quit = false;
-
-			//Event handler
-			SDL_Event e;
-
-			//While app is running
-			while (!quit)
-			{
-				//Handle events on queue
-				while(SDL_PollEvent(&e) != 0)
-				{
-					//User requests quit
-					if (e.type == SDL_QUIT) 
-					{
-						quit = true;
-					}
-				}
-			}
 		}
 
 		//Free resources and close SDL
