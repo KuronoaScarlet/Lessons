7d39f4b44004dfd452c725209a08ec4d774976be
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2020-10-08 14:30:40 +0200

    Handout for Videogames Dev

---
+++ Handout/Game/Source/Module.h
@@ -0,0 +1,76 @@
+#ifndef __MODULE_H__
+#define __MODULE_H__
+
+#include "SString.h"
+
+#include "PugiXml/src/pugixml.hpp"
+
+class App;
+
+class Module
+{
+public:
+
+	Module() : active(false)
+	{}
+
+	void Init()
+	{
+		active = true;
+	}
+
+	// Called before render is available
+	// L01: DONE 5: Sending config file to all modules
+	virtual bool Awake(pugi::xml_node&)
+	{
+		return true;
+	}
+
+
+
+	// Called before the first frame
+	virtual bool Start()
+	{
+		return true;
+	}
+
+	// Called each loop iteration
+	virtual bool PreUpdate()
+	{
+		return true;
+	}
+
+	// Called each loop iteration
+	virtual bool Update(float dt)
+	{
+		return true;
+	}
+
+	// Called each loop iteration
+	virtual bool PostUpdate()
+	{
+		return true;
+	}
+
+	// Called before quitting
+	virtual bool CleanUp()
+	{
+		return true;
+	}
+
+    // L02: TODO 2: Create new virtual methods to Load / Save
+
+	// Called to load the game
+	virtual void LoadGame(pugi::xml_node&) const;
+
+	// Called to save the game
+	virtual void SaveGame(pugi::xml_node&) const;
+
+public:
+
+	SString name;
+	bool active;
+
+};
+
+#endif // __MODULE_H__
